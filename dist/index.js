0fc09a88cb23717ca463bbf414465c6d7649bddc
Abdul-Mannan786 <abdulmannan@cacilian.com>
2023-08-07 08:40:38 +0500

    commit 4

--- dist/index.js
+++ dist/index.js
@@ -2,17 +2,16 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
 const express_1 = __importDefault(require("express"));
 // import {connectRedis} from "./utils/connectRedis";
 // import authRouter from "./src/routes/auth.route";
-const config_1 = __importDefault(require("config"));
 // import { connectRedis } from "./src/connectRedis";
+const config_1 = __importDefault(require("./config/config"));
 const app = (0, express_1.default)();
 // app.use("/api/auth", authRouter);
-// connectRedis();
-console.log(config_1.default.get('dbName'));
 app.get("/", (req, res, next) => {
     res.status(200).json({
         status: "success",
@@ -25,7 +24,7 @@ app.get("/api/healthChecker", (req, res, next) => {
         message: "healthChecker",
     });
 });
-const port = 4000;
+const port = ((_a = config_1.default === null || config_1.default === void 0 ? void 0 : config_1.default.app) === null || _a === void 0 ? void 0 : _a.port) || 4000;
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
     // ? call the connectDB function here
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,11 @@ require("dotenv").config();
 import express, { NextFunction, Request, Response } from "express";
 // import {connectRedis} from "./utils/connectRedis";
 // import authRouter from "./src/routes/auth.route";
-import config from 'config';
 // import { connectRedis } from "./src/connectRedis";
+import config from "./config/config";
 const app = express();
 
 // app.use("/api/auth", authRouter);
-
-// connectRedis();
-console.log(config.get<string>('dbName'))
 app.get(
   "/",
   (req: Request, res: Response, next: NextFunction) => {
@@ -30,7 +27,7 @@ app.get(
   }
 );
 
-const port = 4000;
+const port = config?.app?.port || 4000;
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
   // ? call the connectDB function here
