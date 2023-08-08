11bf2f125e57aa6aecdbeefcd9c94c12c90874f4
Abdul-Mannan786 <abdulmannan@cacilian.com>
2023-08-08 05:49:27 +0500

    commit new-3

--- dist/routes/auth.route.js
+++ dist/routes/auth.route.js
@@ -1,51 +1,37 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-// import {
-//   loginHandler,
-//   logoutHandler,
-//   refreshHandler,
-//   registerHandler,
-// } from "../controllers/auth.controller";
-// import validate from "../schemas/schema.validate";
-// import {
-//   createUserSchema,
-//   loginUserSchema,
-// } from "../schemas/schema.createUser";
-// import { authenticate } from "../middleware/deserializeUser";
-// import { requireUser } from "../middleware/requireUser";
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
+const auth_controller_1 = require("../controllers/auth.controller");
+const schema_validate_1 = __importDefault(require("../schemas/schema.validate"));
+const schema_createUser_1 = require("../schemas/schema.createUser");
+const deserializeUser_1 = require("../middleware/deserializeUser");
+const requireUser_1 = require("../middleware/requireUser");
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
 const router = express_1.default.Router();
-router.post("/login", (req, res, next) => {
-    res.json({ user: req.body });
-});
-router.get("/register", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    const user = yield prisma.user.findMany({
-        select: {
-            username: true,
-            email: true,
-            role: true,
-        },
-    });
-    res.json({ user: user });
-}));
-// router.post("/register", validate(createUserSchema), registerHandler);
-// router.post("/login", validate(loginUserSchema), loginHandler);
+// router.post("/login",  (req: Request, res: Response, next: NextFunction) => {
+//   res.json({ user: req.body });
+// });
+// router.get(
+//   "/register",
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     const user = await prisma.user.findMany({
+//       select: {
+//         username: true,
+//         email: true,
+//         role: true,
+//       },
+//     });
+//     res.json({ user: user });
+//   }
+// );
+router.post("/register", (0, schema_validate_1.default)(schema_createUser_1.createUserSchema), auth_controller_1.registerHandler);
+router.post("/login", (0, schema_validate_1.default)(schema_createUser_1.loginUserSchema), auth_controller_1.loginHandler);
 // router.use(authenticate, requireUser);
-// router.get("/refresh", refreshHandler);
-// router.get("/logout", logoutHandler);
+router.get("/refresh", deserializeUser_1.authenticate, requireUser_1.requireUser, auth_controller_1.refreshHandler);
+router.get("/logout", deserializeUser_1.authenticate, requireUser_1.requireUser, auth_controller_1.logoutHandler);
 exports.default = router;
 //# sourceMappingURL=auth.route.js.map
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,47 +1,48 @@
 import express, { Request,Response,NextFunction } from "express";
-// import {
-//   loginHandler,
-//   logoutHandler,
-//   refreshHandler,
-//   registerHandler,
-// } from "../controllers/auth.controller";
-// import validate from "../schemas/schema.validate";
-// import {
-//   createUserSchema,
-//   loginUserSchema,
-// } from "../schemas/schema.createUser";
-// import { authenticate } from "../middleware/deserializeUser";
-// import { requireUser } from "../middleware/requireUser";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import {
+  loginHandler,
+  logoutHandler,
+  refreshHandler,
+  registerHandler,
+} from "../controllers/auth.controller";
+import validate from "../schemas/schema.validate";
+import {
+  createUserSchema,
+  loginUserSchema,
+} from "../schemas/schema.createUser";
+import { authenticate } from "../middleware/deserializeUser";
+import { requireUser } from "../middleware/requireUser";
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
 
 const router = express.Router();
 
-router.post("/login",  (req: Request, res: Response, next: NextFunction) => {
-  res.json({ user: req.body });
-});
+// router.post("/login",  (req: Request, res: Response, next: NextFunction) => {
+//   res.json({ user: req.body });
+// });
 
-router.get(
-  "/register",
-  async (req: Request, res: Response, next: NextFunction) => {
-    const user = await prisma.user.findMany({
-      select: {
-        username: true,
-        email: true,
-        role: true,
-      },
-    });
-    res.json({ user: user });
-  }
-);
-// router.post("/register", validate(createUserSchema), registerHandler);
+// router.get(
+//   "/register",
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     const user = await prisma.user.findMany({
+//       select: {
+//         username: true,
+//         email: true,
+//         role: true,
+//       },
+//     });
+//     res.json({ user: user });
+//   }
+// );
 
-// router.post("/login", validate(loginUserSchema), loginHandler);
+router.post("/register", validate(createUserSchema), registerHandler);
+
+router.post("/login", validate(loginUserSchema), loginHandler);
 
 // router.use(authenticate, requireUser);
 
-// router.get("/refresh", refreshHandler);
+router.get("/refresh" ,authenticate, requireUser, refreshHandler);
 
-// router.get("/logout", logoutHandler);
+router.get("/logout",authenticate, requireUser, logoutHandler);
 
 export default router;
