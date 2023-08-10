c517d40a6faa84befce08be211317ead4dec78e4
Abdul-Mannan786 <abdulmannan@cacilian.com>
2023-08-11 03:52:13 +0500

    update dist folder

--- dist/middleware/corsOptions.js
+++ dist/middleware/corsOptions.js
@@ -1,15 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const globalVal_1 = require("@utils/globalVal");
-exports.default = {
-    origin: (origin, callback) => {
-        if (globalVal_1.allowedOrigins.indexOf(origin) !== -1 || !origin) {
+const corsOptions = {
+    origin: (origin = '', callback = () => { }) => {
+        if (globalVal_1.allowedOrigins.indexOf(origin !== null && origin !== void 0 ? origin : '') !== -1 || !origin) {
             callback(null, true);
         }
         else {
-            callback(new Error('Not allowed by CORS'));
+            callback(new Error('Not allowed by CORS'), false);
         }
     },
     optionsSuccessStatus: 200,
 };
+exports.default = corsOptions;
 //# sourceMappingURL=corsOptions.js.map
